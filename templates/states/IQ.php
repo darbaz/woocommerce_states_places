f41ddea0e2f2c256628a5261bcb57c684cd74198
Darbaz Dara <25202708+darbaz@users.noreply.github.com>
2021-06-08 13:15:12 +0300

    Update and rename XX.php to IQ.php

---
+++ templates/states/IQ.php
@@ -0,0 +1,37 @@
+<?php
+
+/**
+ * $country states/provinces/regions
+ * - $count states/provinces/regions
+ * 
+ * Source: 
+ * - [Please add the source(s) link(s) to check the list of places]
+ *
+ * @author  Darbaz Dara <darbazdara@hotmail.com> | https://darbazdara.com
+ * @version 1.0.0
+ * @license http://www.gnu.org/licenses/gpl-2.0.html
+ */
+
+global $states;
+$states['IQ'] = array(
+  // https://en.wikipedia.org/wiki/Governorates_of_Iraq
+  'AN' => 'Al Anbar',
+  'BB' => 'Babylon',
+  'BG' => 'Baghdad',
+  'BA' => 'Basrah',
+  'DQ' => 'Dhi Qar',
+  'QA' => 'Al-Qādisiyyah',
+  'DI' => 'Diyala',
+  'DA' => 'Duhok',
+  'AR' => 'Erbil',
+  'HA' => 'Halabja',
+  'KA' => 'Karbala',
+  'KI' => 'Kirkuk',
+  'MA' => 'Maysan',
+  'MU' => 'Muthanna',
+  'NA' => 'Najaf',
+  'NI' => 'Nineveh',
+  'SD' => 'Saladin',
+  'SU' => 'Sulaymaniyah',
+  'WA' => 'Wasit',
+);
